26d9cde166f0fb5b5bc4c00b4c9d21e126a82573
Codeboy <danilrogal@vk.com>
2025-09-09 13:55:54 +0700

    Add Lab 1: Greeting program with CI/CD and Checkstyle

---
+++ Main.java
@@ -0,0 +1,17 @@
+package org.example.lab1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        System.out.print("> ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
+        scanner.close();
+    }
+}
